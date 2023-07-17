2773239ed41874aaa569a853b7326d47a412cd27
Andrey O <ozzy.ext@gmail.com>
2023-07-17 22:24:35 +0300

    Add options

---
+++ src/MyLab.Search.EsDbSync/SyncOptions.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLab.Search.EsDbSync
+{
+    class SyncOptions
+    {
+        public IndexNamingOptions IndexNaming { get; set; } = new();
+        public string SeedPath { get; set; } = "/var/lib/es-db-sync/seeds";
+        public string ScriptPath { get; set; } = "/etc/es-db-sync/scripts";
+        public int SyncPageSize { get; set; } = 500;
+    }
+
+    class IndexNamingOptions
+    {
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+    }
+}
